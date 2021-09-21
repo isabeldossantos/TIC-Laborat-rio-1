a8580644630312463e963d97f202ff610ce855b6
Ernesto Cabingano Salias <ernesto.cabingano.salias@gmail.com>
2021-09-21 23:48:14 +0100

    GitHub

---
+++ tic6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int nc;
+	float pc,cesp;
+	printf("informe o preco do convites");
+	scanf("%f",&cesp);
+	nc=cesp/pc;
+	printf("o numero de convites que devem ser vendidos e de%i",nc);
+	
+
+} 
